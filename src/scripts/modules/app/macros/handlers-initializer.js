c569fe314623ce04fe02f2d625d6e7ea2bc42165
Kaloyan Bozhkov <Kaloyan.Bozhkov@mwlsystems.co.uk>
2019-08-14 14:50:52 +0100

    Started work on Previous Entries

--- src/scripts/modules/app/macros/handlers-initializer.js
+++ src/scripts/modules/app/macros/handlers-initializer.js
@@ -1,14 +1,14 @@
 import caloriesHandler from './handlers/calories-handlers';
 import servingsHandlers from './handlers/servings-handlers';
 import dailyEntriesHandlers from './handlers/daily-entries-handlers';
-import historyEntriesHandlers from './handlers/history-entries-handlers';
+import previousEntriesHandlers from './handlers/previous-entries-handlers';
 import graphHandlers from './handlers/graph-handlers';
 
 export default () => {
     caloriesHandler();
     servingsHandlers();
     dailyEntriesHandlers();
-    historyEntriesHandlers();
+    previousEntriesHandlers();
     graphHandlers();
     console.log("Handlers for macros section initialized");
 }
